48532edfb1702c995a4b3069b9bb2510057f07e2
Kliealy <2185127779@qq.com>
2022-12-08 15:13:17 +0800

    12.8

--- C/first.cpp
+++ C/first.cpp
@@ -1,21 +1,8 @@
-#include<cstdio>
-//定义一个宏 建议大写 和普通变量区分开
-#define PI 3.1415926
+#include  <cstdio>
 
-int main(int argc,char *argv[])
+int main()
 {
-    float r=0.0f;//定义一个r变量 系统给r开辟4字节空间
-    float area = 0.0f;//定义一个面积变量
-
-    //获得半径 从键盘获得scan
-    printf("请输入圆的半径r:");
-    scanf("%f", &r);//带阻塞
-
-    //算法:计算面积area = 3.14 * r * r
-    area = PI*r*r;
-
-    //将面积输出%.2f中的.2表示小数部分保留两位
-    printf("area = %.2f\n",area);
-
+    printf("5*5=") ;
     return 0;
+
 }
--- Python/first.py
+++
@@ -1,7 +0,0 @@
-print('请输入一个数字：')
-n = int(input())
-d = dict()
-for i in range(1, n + 1):
-    d[i] = i * i
-
-print(d)
