1f72383e7289ed289d61414a4fe6ad933e311d34
Kliealy <2185127779@qq.com>
2022-12-18 20:04:27 +0800

    12.18

--- Python/second.py
+++ Python/second.py
@@ -1,3 +1,23 @@
-import keyword
+import random
 
-print(keyword.kwlist)
+guessTaken = 0
+print("Hello!What's your name?")
+User_name = input()
+number = random.randint(1, 21)
+print("Well," + User_name + "I am thinking of a number between 1 and 20.\nTake a guess.")
+
+while guessTaken < 6:
+    print("Take a guess.")
+    guess = input("请输入0到20的数字：")
+    guess = int(guess)
+
+    guessTaken += 1
+
+    if guess < number:
+        print("Your guess is too low")
+
+    if guess > number:
+        print("Your guess is too high")
+
+    else: .IxF:44#hShY\MY-010454
+         print("Congratulation!{0}You are right!".format(User_name))
