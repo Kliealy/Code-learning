e8ad2eb155db1dc7a3ee4bff79954ab3003391a8
Kliealy <2185127779@qq.com>
2022-12-18 12:52:14 +0800

    12.18

--- Python/first.py
+++ Python/first.py
@@ -1,11 +1,12 @@
 import requests
 
-m_url = 'https://dl.stream.qqmusic.qq.com/C400004Rhcva2nyTJ0.m4a?guid=3801622700&vkey=A35E21D4D91029D65DB70135AA41DD4F8134150A3C95972CDA697456D8D77F0B66A8E7049B90FC600E81F2E076F8528A2E6CC7DEC8735020&uin=482165350&fromtag=120032'
+m_url = 'https://dl.stream.qqmusic.qq.com/C400003pyeir0FV236.m4a?guid=9892066488&vkey=3E2FF712482A60B3C9EB32F1E75DE7770F3CFB47489C806F690428457532B73E393953D4DC970EF946CA9F7059422B8035EBE7EF2197FCC0&uin=482165350&fromtag=120032'
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.0.0'
 }
-m_resp = requests.get(m_url, headers=headers)
-with open('zzz.mp3', 'wb') as f:
-    f.write(m_resp.content)
 
+m_resp = requests.get(m_url, headers=headers)
+
+with open('The Night.mp3', 'wb') as f:
+    f.write(m_resp.content)
