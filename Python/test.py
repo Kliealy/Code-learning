96478f3a7a7908d5e8642f5794be06f189cd6370
Kliealy <2185127779@qq.com>
2022-12-19 19:05:53 +0800

    12.19

--- Python/test.py
+++ Python/test.py
@@ -1,4 +1,5 @@
 import urllib.request
 
-response = urllib.request.urlopen('https://www.python.org')
+response = urllib.request.urlopen('https://www.mjtv.me/index.php/vod/play/id/6290/sid/1/nid/10.html')
 print(response.read().decode('utf-8'))
+
